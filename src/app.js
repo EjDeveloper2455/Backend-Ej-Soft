b6bbf7c0ba0bb68a73bd2aa818a5c5e66e682574
EjDeveloper2455 <ejdeveloper55@gmail.com>
2023-05-18 17:40:35 -0600

    Modificacion 1.3

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import morgan from 'morgan';
+import userRoutes from './routes/user.routes';
+
 
 const app = express();
 //app.use(express.json());
@@ -8,6 +10,9 @@ app.set('port',8080);
 
 //Middlewares
 app.use(morgan('dev'));
+
+//Rutes
+app.use(userRoutes);
 
 
 export default app;
