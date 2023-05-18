e6b64c5bdf2c07c90c939fc744e3130c4ef7d50a
EjDeveloper2455 <ejdeveloper55@gmail.com>
2023-05-18 01:20:39 -0600

    Mi servidor inicial

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+
+app.get('/inicioServidor', (req, res) => {
+    res.send('El servidor funciona correctamente');
+});
+
+app.listen(3000, ()=>{
+    console.log('El servidor se ha iniciado en el puerto 3000');
+});
