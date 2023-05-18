b67be6813fcec281a3e0a6c74a80e5163ebaada5
EjDeveloper2455 <ejdeveloper55@gmail.com>
2023-05-18 02:31:50 -0600

    Nueva actualizacion 1.0

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const app = express();
 app.use(express.json());
 
 app.get('/inicioServidor', (req, res) => {
-    res.send('El servidor funciona correctamente');
+    res.send('<h1>El servidor funciona correctamente<h1>');
 });
 
 app.listen(3000, ()=>{
-    console.log('<h1>El servidor se ha iniciado en el puerto 3000</h1>');
+    console.log('El servidor se ha iniciado en el puerto 3000');
 });
