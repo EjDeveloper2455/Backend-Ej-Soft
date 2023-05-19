a8689fec806b5ef9998803eb133c28e56164a010
EjDeveloper2455 <ejdeveloper55@gmail.com>
2023-05-18 19:48:17 -0600

    Actualizacion 1.6

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 import {config} from 'dotenv'
 
 config();
-
+console.log(process.env.HOST+" - "+process.env.DATABASE+" - "+process.env.USER+" - "+process.env.PASSWORD);
 export default {
     host: process.env.HOST || "localhost",
     database: process.env.DATABASE || "bd_ej_soft",
