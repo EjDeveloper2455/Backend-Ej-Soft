05a3bf8e8331e6ca0a92ce54ebc4841f50a44c6b
EjDeveloper2455 <ejdeveloper55@gmail.com>
2023-05-18 19:44:33 -0600

    Actualizacion 1.5

--- src/config.js
+++ src/config.js
@@ -3,8 +3,8 @@ import {config} from 'dotenv'
 config();
 
 export default {
-    host: process.env.HOST || "",
-    database: process.env.DATABASE || "",
-    user: process.env.USER || "",
-    password: process.env.PASSWORD
+    host: process.env.HOST || "localhost",
+    database: process.env.DATABASE || "bd_ej_soft",
+    user: process.env.USER || "root",
+    password: process.env.PASSWORD || "Katty-123"
 }
