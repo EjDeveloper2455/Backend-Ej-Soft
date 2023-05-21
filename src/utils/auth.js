c664d15193b9647baf8a45f72c16b4c6d8f5aec2
EjDeveloper2455 <ejdeveloper55@gmail.com>
2023-05-20 22:32:21 -0600

    Actualizacion 2.0

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,3 +1,4 @@
+import { json } from 'express';
 import {getConnection} from '../database/database'
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
@@ -16,12 +17,20 @@ const getUser = async (req,res) =>{
 
 const signUp = async (req,res) =>{
     try{
-        const {email,password,rol} = req.body;
+        var {email,password,rol} = req.body;
+
         const salt = await bcrypt.genSalt(10);
         password = await bcrypt.hash(password, salt);
-        //const result = await connection.query("Select * from tbl_usuario");
-        res.json(req.body);
+        const connection = await getConnection();
+        const result = await connection.query("INSERT INTO `tbl_usuario` (`usuario_email`, `usuario_password`, `usuario_rol`) VALUES (?, ?, ?); ",[email,password,rol]);
+        if(result){
+            const payload = {email: email.email,role: rol};
+            
+            const token = jwt.sign(payload, SECRET_KEY);
+            res.json({email,rol,token});
+        }
     }catch(err){
+        console.log(err);
         res.status(500).send(err.message);
     }
 };
@@ -30,17 +39,19 @@ const login = async(req, res) =>{
         const {email,password} = req.body;
         const connection = await getConnection();
         const result = await connection.query("Select usuario_email as email, usuario_password as password"
-        +", usuario_rol as rol from tbl_usuario where usuario_email = "+email+" and usuario_password = "+password+";");
-        //console.log(user.email+" - "+user.password);
-        /*if(!result){
-            res.status(401).send('Email o contraseña incorrecta');
-            return;
-        }*/
-        //const isMatch = await bcrypt.compare(password, result.password);
+        +", usuario_rol as rol from tbl_usuario where usuario_email = '"+email+"';");
         if(!result){
             res.status(401).send('Email o contraseña incorrecta');
+            return;
+        }
+        console.log(password);
+        const isMatch = await bcrypt.compare(password, result[0].password);
+        if(!isMatch){
+            res.status(401).send('Email o contraseña incorrecta');
         }else{
-            const payload = {email: result.email,role: result.rol};
+            const user = {"Email": result[0].email,"Rol": result[0].rol};
+            const payload = {user};
+            
             const token = jwt.sign(payload, SECRET_KEY);
             res.json({user, token});
         }
